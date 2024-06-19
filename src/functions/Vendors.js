83cdf46e524988c828f658b4abadca7aa172789f
Justin Alaan-Nguyen <justinalaan2@gmail.com>
2024-06-19 02:20:16 -0400

    Updated CRUD Products

--- src/functions/Vendors.js
+++ src/functions/Vendors.js
@@ -2,83 +2,108 @@ const axios = require("axios");
 import { DEBUG, API_URL } from "../../config";
 
 module.exports = {
-    getAll: async () => {
-        let url = `${API_URL[DEBUG]}/api/vendors/getAll`;
-    
-        try {
-          let response = await axios.get(url, {
-            headers: {
-              "Content-Type": "application/json",
-            },
-          });
-    
-          if (response.status === 200) {
-            return response.data
-          }
-        } catch (error) {
-          return error;
-        }
-      },
+  getAll: async () => {
+    let url = `${API_URL[DEBUG]}/api/vendors/getAll`;
 
-    create: async (vendor) => {
-        try {
-            if(!vendor) {
-                return ("All fields required");
-            }
+    try {
+      let response = await axios.get(url, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
-            let reqOptions = {
-                url: `${API_URL[DEBUG]}/api/vendors/create`,
-                method: "POST",
-                data: {
-                    vendor: vendor
-                },
-                headers: {
-                    "Content-Type": "application/json", 
-                },
-            };
-            
-            let response = await axios.request(reqOptions);
-            return(response);
-        } catch (error) { 
-            return(error);
-        }
-    },
-
-    delete: async (application) => {
-      try {
-        console.log(application)
-        const response = await axios.post(
-          `${API_URL[DEBUG]}/api/vendors/delete`,
-          {
-            application: application, // Pass the application data in the body
-          }
-        );
-    
-        // Handle different response status codes
-        if (response.status === 200) {
-          return response;
-        } else {
-          console.error("Unexpected status code:", response.status);
-          // Handle other unexpected status codes
-        }
-      } catch (error) {
-        // Handle network errors or other issues
-        console.error("Error deleting application: ", error);
+      if (response.status === 200) {
+        return response.data;
       }
-    },
-
-    approve: async (application_id) => {
-      try {
-        const response = await axios.post(
-          `${API_URL[DEBUG]}/api/vendors/approve`,
-          {
-            application_id: application_id
-          }
-        );
-
-        return response;
-      } catch (error) {
-        return error
-      }
+    } catch (error) {
+      return error;
     }
-}
+  },
+
+  create: async (vendor) => {
+    try {
+      if (!vendor) {
+        return "All fields required";
+      }
+
+      let reqOptions = {
+        url: `${API_URL[DEBUG]}/api/vendors/create`,
+        method: "POST",
+        data: {
+          vendor: vendor,
+        },
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      let response = await axios.request(reqOptions);
+      return response;
+    } catch (error) {
+      return error;
+    }
+  },
+
+  delete: async (application) => {
+    try {
+      console.log(application);
+      const response = await axios.post(
+        `${API_URL[DEBUG]}/api/vendors/delete`,
+        {
+          application: application, // Pass the application data in the body
+        }
+      );
+
+      // Handle different response status codes
+      if (response.status === 200) {
+        return response;
+      } else {
+        console.error("Unexpected status code:", response.status);
+        // Handle other unexpected status codes
+      }
+    } catch (error) {
+      // Handle network errors or other issues
+      console.error("Error deleting application: ", error);
+    }
+  },
+
+  approve: async (application_id) => {
+    try {
+      const response = await axios.post(
+        `${API_URL[DEBUG]}/api/vendors/approve`,
+        {
+          application_id: application_id,
+        }
+      );
+
+      return response;
+    } catch (error) {
+      return error;
+    }
+  },
+
+  getVendorByAccountId: async (accountId) => {
+    if (!accountId) {
+      console.error("No account ID provided");
+      return null;
+    }
+
+    console.log("(vendors.js)looking for vendor with acc id:", accountId);
+    let url = `${API_URL[DEBUG]}/api/vendors/getVendorByAccountId?account_id=${accountId}`;
+
+    try {
+      let response = await axios.get(url, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.status === 200) {
+        return response.data;
+      }
+    } catch (error) {
+      console.error("(vendors.js)Error fetching vendor data", error);
+      return null;
+    }
+  },
+};
