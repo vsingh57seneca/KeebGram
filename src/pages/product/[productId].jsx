975a22daf26d47e2ecb9d5d32c22e33e324c24ec
Justin Alaan-Nguyen <justinalaan2@gmail.com>
2024-06-19 21:28:31 -0400

    user's vendor_id must match products vendor_id to view product details

--- src/pages/product/[productId].jsx
+++ src/pages/product/[productId].jsx
@@ -1,6 +1,8 @@
 import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
-import React from "react";
+import { useAtom } from "jotai";
+import React, { useEffect, useState } from "react";
+import { userAtom } from "../../../store";
 
 const ProductDetails = dynamic(
   () => import("../../components/products/ProductDetails"),
@@ -12,12 +14,20 @@ const ProductDetails = dynamic(
 const ProductPage = () => {
   const router = useRouter();
   const { productId } = router.query;
+  const [user, setUser] = useAtom(userAtom);
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
 
   if (!productId) {
     return <div>Loading...</div>;
   }
 
-  return <ProductDetails />;
+  return <ProductDetails user={user} />;
 };
 
 export default ProductPage;
